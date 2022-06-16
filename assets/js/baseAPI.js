642d299331105e38855d923c58674e64121a0e5d
Ayoo <Hazell55@163.com>
2022-06-16 17:49:22 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,21 @@ $(function(){
     // 拿到 URL 数据后，重新对 URL 进行拼接，加上根路径，再重新赋值给 options.url
     $.ajaxPrefilter(function(options){
         options.url = "http://www.liulongbin.top:3007"+options.url
+
+        // 统一为有权限的接口，设置 headers 请求头
+        if(options.url.indexOf('/my/') !== -1){
+            options.headers={
+                Authorization:localStorage.getItem('token') || ''
+    
+            }
+        }
+
+        options.complete = function(res){
+            if(res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！"){
+                localStorage.removeItem('token')
+                location.href = '/素材/login.html'
+            }
+        }
+        
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,71 @@
+$(function(){
+    getUserInfo()
+
+    let layer = layui.layer
+    // 点击退出
+    $('.btnLogout').on('click',function(){
+        layer.confirm('确认退出登录？', {icon: 3, title:'提示'}, function(index){
+            // 清空本地存储的token
+            localStorage.removeItem('token')
+            // 跳转登录页面
+            location.href = '/素材/login.html'
+            // 关闭 confirm 询问框
+            layer.close(index)
+
+          });
+        
+    })
+
+})
+
+
+// 获取用户信息
+function getUserInfo(){
+    $.ajax({
+        method:'get',
+        url:'/my/userinfo',
+        // headers:{
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
+        success:function(res){
+            if(res.status !== 0){
+                return layui.layer.msg('获取信息失败')
+            }
+            // 渲染头像,调用函数
+            renderAvatar(res.data)
+        },
+        // 不论是成功还是失败，最终都会调用 complete 函数
+        // 写到 baseAPI 中
+        // complete:function(res){
+        //     // console.log(res);
+        //     if(res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！"){
+        //         localStorage.removeItem('token')
+        //         location.href = '/素材/login.html'
+        //     }
+        // }
+
+    })
+}
+
+
+// 渲染头像
+function renderAvatar(user){
+    // 获取用户姓名
+    let username = user.nickname || user.username
+    // 设置欢迎的文本内容
+    $('.welcome').html('欢迎 &nbsp'+username)
+    // 渲染图片头像
+    if(user.user_pic !== null){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').html(first).hide()
+    }
+    // 渲染文本头像
+    else{
+        // 文本的第一个字符，转为大写
+        let first = username[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+        $('.layui-nav-img').attr('src',user.user_pic).hide()
+
+    }
+
+}
